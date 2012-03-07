94705f384d0beb99010d6873cd74f4a0a2dd4acd
Bjorn Winckler <bjorn.winckler@gmail.com>
2012-03-07 11:21:42 +0100

    Replace readAndCombineLxbs() with readManyLxbs()

--- R/lxb.R
+++ R/lxb.R
@@ -7,23 +7,8 @@ readLxb <- function (filename) {
     x
 }
 
-readAndCombineLxbs <- function(paths) {
-    # Read multiple LXB files and combine into one matrix.  It is assumed that
-    # all LXB files have the same number of columns (with the same names).
+readManyLxbs <- function(paths) {
+    # Read multiple LXB files and return a list of matrices (one for each LXB).
     filenames <- Sys.glob(paths)
-
-    # Option 1: Output diagnostic messages while reading
-    # n   <- length(filenames)
-    # res <- vector("list", n)
-    # for (k in 1:n) {
-    #     name <- filenames[k]
-    #     message(c("Reading file [", k, " of ", n, "]: ", name))
-    #     res[[k]] <- readLxb(name)
-    # }
-
-    # Option 2: No diagnostics
-    res <- lapply(filenames, readLxb)
-
-    # Combine list of matrices into one big matrix
-    do.call(rbind, res)
+    lapply(filenames, readLxb)
 }
